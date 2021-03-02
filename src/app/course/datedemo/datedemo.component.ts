0b3452b9d6c82b76f14667a36295525bbb35c89b
Sushant Hulawale <sushanthulawale@gmail.com>
2021-03-02 11:30:48 +0530

    final changes

--- src/app/course/datedemo/datedemo.component.ts
+++ src/app/course/datedemo/datedemo.component.ts
@@ -1,24 +1,31 @@
 import { Component, OnInit } from '@angular/core';
+import { FormArray, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'datedemo',
   templateUrl: './datedemo.component.html',
   styleUrls: ['./datedemo.component.scss']
 })
-export class DatedemoComponent implements OnInit {
-  today;
-    
+export class DatedemoComponent {
+  today;  
+  degree=null;
   constructor() {
     this.showdate();
   }
+  form= new FormGroup({
+    topics: new FormArray([])
+ });  
 
   showdate(){
     this.today = new Date();
     return this.today;
   }
   
-  ngOnInit(): void {
-   
+  convertTemp(){
+    console.log(this.degree);
+  
   }
+  
+
   
 }
